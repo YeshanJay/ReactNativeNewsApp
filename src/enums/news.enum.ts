8e8eebb9226d49d112067924a6323fe03566119a
Yeshan Jayasinghe <yeshankj@gmail.com>
2019-11-26 22:25:38 +0530

    Developed services.

---
+++ src/enums/news.enum.ts
@@ -0,0 +1,10 @@
+
+export enum NEWS_CATEGORY_ENUM {
+    BUSINESS = "business",
+    ENTERTAINMENT = "entertainment",
+    GENERAL = "general",
+    HEALTH = "health",
+    SCIENCE = "science",
+    SPORTS = "sports",
+    TECHNOLOGY = "technology"
+}
